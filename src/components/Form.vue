51bdc3bb1b078af947b3f26b7fa4f0d6a6734c76
Judison Godinho de Sousa <judison@outlook.com>
2019-11-04 11:42:07 -0300

    first commit

---
+++ src/components/Form.vue
@@ -0,0 +1,6 @@
+<template>
+    <form name="form"  id="formulario" class="form-inline">
+		<input type="text" name="tarefa" class="form-control" placeholder="Adicione uma tarefa aqui...">
+		<input type="button" onclick="adicionar()" value="Add" class="btn btn-primary">
+	</form>
+</template>
